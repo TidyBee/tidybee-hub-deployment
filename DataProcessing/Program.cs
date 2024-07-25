64cda6b914ff5e04d74b25bf1ee3f007a4bb4337
Blynqs <baptiste.friboulet@epitech.eu>
2024-07-26 01:20:53 +0900

    minor fix

--- DataProcessing/Program.cs
+++ DataProcessing/Program.cs
@@ -2,8 +2,6 @@ using Microsoft.EntityFrameworkCore;
 using DataProcessing.Context;
 using DataProcessing;
 using Microsoft.Extensions.Hosting.WindowsServices;
-using Microsoft.EntityFrameworkCore;
-using DataProcessing.Context;
 
 var builder = WebApplication.CreateBuilder(args);
 
